3e30be0760702a5a02d54b4bd37692df23e6aa86
Ganesh-DevOps-Eng <ganeshjatms@gmail.com>
2023-10-10 13:09:21 +0530

    Update bastion.tf

--- RDS-Module/bastion.tf
+++ RDS-Module/bastion.tf
@@ -13,3 +13,12 @@ resource "aws_instance" "bastion" {
 
   key_name = aws_key_pair.bastion_pair.key_name
 }
+
+resource "aws_instance" "bastion" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = module.VPC-Module.subnet-private[0]
+  vpc_security_group_ids = [module.VPC-Module.bastion_sg]
+
+  key_name = aws_key_pair.bastion_pair.key_name
+}
